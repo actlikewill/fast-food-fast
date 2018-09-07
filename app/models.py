f9e33b39bb9c43b8821123d0726a325440e8979d
Wilson Gaturu <wilsongaturu@gmail.com>
2018-09-07 15:15:11 +0300

    [feature #160318085] : Created the endpoint for GET /api/v1/orders/<orderId> [#160323041]

--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
 class Order:
-    def __init__(self, item_name, item_quantity):
+    def __init__(self, item_name, item_quantity):        
         self.item_name = item_name        
         self.item_quantity = item_quantity
+        self.order_no = 1
 
 
 
@@ -11,8 +12,12 @@ class Orders:
 
     def add_order(self, order_obj):
         new_order_obj = Order(order_obj['item'], order_obj['quantity'])
+
+        order_number = len(self.order_list) +1
+
         new_order = {
             "item": new_order_obj.item_name,
-            "quantity": new_order_obj.item_quantity
+            "quantity": new_order_obj.item_quantity,
+            "orderId": order_number
         }        
         self.order_list.append(new_order)
