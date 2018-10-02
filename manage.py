65c8e3f0f3e66c24bd0120354766fbd6914175a1
actlikewill <wilsongaturu@gmail.com>
2018-10-02 16:25:51 +0300

    [feature : #160752560] Created the user authentication module The endpoints created are GET / auth/users, POST /auth/users and POST / auth/login [#160814187]

--- manage.py
+++ manage.py
@@ -1,9 +1,11 @@
-"""This creates runs the application"""
+"""This runs the application"""
 
 from flask_script import Manager
 from app import create_app
+from flask_jwt_extended import JWTManager
 
 APP = create_app('default')
+JWT = JWTManager(APP)
 MGR = Manager(APP)
 
 if __name__ == '__main__':
