9f200f50f56b5f47a93018b91ff4c88d4c8649ae
Wilson Gaturu <wilsongaturu@gmail.com>
2018-10-28 16:48:38 +0300

    testing jasmine

--- app/api/auth/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-This initializes the blueprint for authorizing and genrating tokens
-"""
-from flask import Blueprint
-
-AUTH = Blueprint('auth', __name__)
-
-from . import user_views
--- app/api/v1/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-This initializes the blueprint for version 1 of the api
-"""
-from flask import Blueprint
-
-API_V1 = Blueprint('api_v1', __name__)
-
-from .orders import order_views
-from .menu import menu_views
